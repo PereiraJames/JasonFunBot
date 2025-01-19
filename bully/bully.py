b88ae9b7812a0781150c8fdf26f54ec684ab0181
PereiraJames <jamespereira_5@hotmail.com>
2025-01-19 19:59:31 +0800

    addition of bully module

--- bully/bully.py
+++ bully/bully.py
@@ -1,2 +1,198 @@
-def test():
-    print("ASDAOSD")
+import random
+import csv
+import datetime
+import os
+import jasontools
+import asyncio
+from openai import OpenAI
+from telegram import Update, Bot
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackContext 
+
+BULLYSTATE = True
+
+BULLYTOLERANCE = 10
+
+import mysql.connector
+import jasontools
+
+db_config = jasontools.parseEnvFile("DB_CONFIG")
+
+def insertUser(username,userid,whitelisted="None",blacklisted="None",realname="nameless"):
+    
+    query = """
+        INSERT INTO telegramUsers (username, userid, whitelisted, blacklisted, realname)
+        VALUES (%s,%s,%s,%s,%s)
+    """
+
+    db = mysql.connector.connect(**db_config)
+    cursor = db.cursor()
+
+    cursor.execute(query, (
+        username, userid, whitelisted, blacklisted, realname
+    ))
+    
+    print(f"Imported {username} | {realname} | {userid}")
+
+    db.commit()
+
+def findUser(username):
+    
+    query = """
+        SELECT *
+        FROM telegramUsers
+        WHERE username = (%s)
+    """
+
+    db = mysql.connector.connect(**db_config)
+    cursor = db.cursor()
+
+    cursor.execute(query, (username,))
+
+    response = cursor.fetchall()
+
+    userData = []
+
+    for row in response:
+        rowData = {
+            "id" : row[0],
+            "username" : row[1],
+            "userid" : row[2],
+            "whitelistsed" : row[3],
+            "blacklisted" : row[4],
+            "realname" : row[5],
+            "profileDesc" : row[6]
+        }
+
+        userData.append(rowData)
+
+    cursor.close()
+    db.close()
+
+    if userData:
+        return userData
+    else:
+        return False
+
+
+def updateUser(username,userid):
+
+    if findUser(username):
+        print("Already in database")
+        return False
+    else:
+        insertUser(username,userid)
+        return True
+
+
+def getInsult(targetsUsername):
+    if BULLYSTATE == False:
+        return None
+
+    userdetails = findUser(targetsUsername)
+
+    if len(userdetails) != 1:
+        print(f"Found {len(userdetails)} in database.")
+        return None
+    else:
+        print(userdetails)
+
+        user_realname = userdetails[0]['realname']
+        user_profileDesc = userdetails[0]['profileDesc']
+
+        generatedInsult = generateInsult(user_realname, user_profileDesc)
+
+        return generatedInsult
+
+def generateInsult(realname,userDesc):
+    chat_prompt = f"Create a short insult for my friend {realname} using this short description of him. You do not need to use all these details, just pick one and make an insult. {userDesc}"
+
+    insult = jasontools.generateChatGPT(chat_prompt)
+
+    return insult   
+
+def get_state():
+    return BULLYSTATE
+
+def set_state(state):
+    global BULLYSTATE
+    if state == True:
+        BULLYSTATE = True
+    else:
+        BULLYSTATE = False
+
+def get_tolerance():
+    return BULLYTOLERANCE
+
+def set_tolerance(level):
+    global BULLYTOLERANCE
+    BULLYTOLERANCE = level
+
+async def checkTarget(update,context):
+
+    message_type: str = update.message.chat.type
+
+    if message_type != "private":
+        return None
+
+    userid = update.message.chat.id
+
+    username = update.message.from_user.username
+
+    if updateUser(username,userid):
+        return True
+    else:
+        return False
+
+async def bullytarget(update,context):
+    ranNum = random.randint(-1, BULLYTOLERANCE)
+
+    if ranNum != 1:
+        return None
+    else:
+        username: str = update.message.chat.username
+
+        getInsult(username)        
+
+async def bullystatus(update,context):
+    bullystate = BULLYSTATE
+
+    if bullystate == True:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Bully Mode Enabled.")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Bully Mode Disabled.")
+
+async def bullyenable(update,context):
+    set_state(True)
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Activating Bully Mode...")
+
+async def bullydisable(update,context):
+    set_state(False)
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Deactivating Bully Mode...")
+
+async def get_bullytolerance(update, context):
+    tolerance = BULLYTOLERANCE
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text= f"Bully Tolerance Levels: {tolerance}")
+
+async def set_bullytolerance(update, context):
+    if context.args: 
+        level = context.args[0]
+
+        if level.isdigit():
+            level = int(level)
+            if level >= 0:
+                set_tolerance(level)
+                await context.bot.send_message(chat_id=update.effective_chat.id, text= f"Bully Tolerance Set to {level}.")
+            else:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text= f"Minimum Level is 0.")
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text= f"Enter a digit please you fool.")
+    else:
+        set_tolerance(5)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text= f"Bully Tolerance Set to {5}.")
+
+
+if __name__ == "__main__":
+    getInsult("Markryann")
--- jasonfunbot.py
+++ jasonfunbot.py
@@ -1,10 +1,15 @@
 import os
 import json
-from bully import bully
+from bully import bully, targetting
 import jasontools
+from telegram import Update, Bot
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackContext
+import random
+import sys
+from datetime import datetime
+import re
 
-
-BOTTOKEN = jasontools.parseEnvFile("BOT_TOKEN")
+BOT_TOKEN = jasontools.parseEnvFile("BOT_TOKEN")
 BOT_USERNAME = jasontools.parseEnvFile("BOT_USERNAME")
 
 MASTER_USERNAME = jasontools.parseEnvFile("TELE_MASTERNAME")
@@ -14,34 +19,123 @@ TELE_DRANKSID = jasontools.parseEnvFile("TELE_DRANKSID")
 
 userstates = {}
 
-def readCommandList(master=False):
-    
-    with open('commands.json') as file:
-        data = json.load(file)
-    
-    # print(data)
+async def helpPeon(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # logAttempts("/help",update)
 
-    if master:
-        commandlist = data['mastercommands']
+    commands = jasontools.readCommandList()
 
-    else:
-        commandlist = data['commands']
+    print(commands)
 
-    allcommands = ""
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=commands)
+
+async def alertMaster(update: Update, context: ContextTypes.DEFAULT_TYPE, alertMessage):
+
+    message_type: str = update.message.chat.type
+
+    username: str = update.message.from_user.username
+
+    if message_type != "private":
+        message_type = update.message.chat.title                 
+
+    alert = f"ALERT: {username} ({message_type}) | {alertMessage}"
+
+    print(alert)
+    await context.bot.send_message(chat_id=HERMES_ID,text=alert)
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    username: str = update.message.chat.username
+
+    message_type: str = update.message.chat.type
+
+    userid = update.message.chat.id
+
+    if message_type != "private":
+        message_type = update.message.chat.title
+
+    text: str = update.message.text
+
+    targetsName = update.message.from_user.username
+    incomingmessage = f'{targetsName} ({update.message.chat.id}) in {message_type}: "{text}"'
+
+    print(incomingmessage, message_type)
+    messagelog(incomingmessage, message_type, targetsName)
+
+    if str(targetsName) != MASTER_USERNAME:
+        messagealert = "Sent: " + "\n" + text
+
+        await alertMaster(update, context, messagealert)
+
+    messagealert = "Sent: " + "\n" + text
+
+    await alertMaster(update, context, messagealert)
+
+    if await bully.checkTarget(update,context):
+        useralert = f"Added {username} ({userid}) to Jason's Pokédex"
+
+        await alertMaster(update, context, useralert)
         
-    for command in commandlist:
-        allcommands += f"{command} | {commandlist[command]}" + "\n"
 
-    print(allcommands)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #Can be used to get user information
+    print(f'Update {update} caused error {context.error}')
+    await alertMaster(update, context, f'Update {update} caused error {context.error}')
+    return
+
+def logAttempts(commandName, update: Update):
     
-readCommandList(False)
+    user = update.effective_user.username
+    
+    message_type: str = update.message.chat.type
 
-# token = jasontools.parseEnvFile("OPENAI_KEY")
+    if message_type != "private":
+        message_type = update.message.chat.title
+    
+    logAttempt = f"{user} used the {commandName} command."
+    
+    messagelog(logAttempt, message_type, user)    
+    alert = f"{user} used the {commandName} command."
+    print(alert)
+    return
 
-# test = jasontools.parseEnvFile("STORY_DB_CONFIG")
+def messagelog(incoming_message, grouptype, person):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    if grouptype == "private":
+        filename = f'messagelogs/{grouptype}/{person}.txt'
+    else:
+        filename = f'messagelogs/{grouptype}/{grouptype}.txt'
+    if not os.path.isfile(filename):
+        print(f"Creating {filename}")
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-# print(token)
-# print(test)
-# print(type(test))
-# print(test['host'])
+    with open(filename, 'a') as file:
+        file.write("(" + current_time + ") " + incoming_message + '\n')
 
+if __name__ == "__main__":
+    print("Jason Intializing...")
+
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    # Normal Commands
+    app.add_handler(CommandHandler('help', helpPeon))
+
+    # MASTER Commands
+    
+    #BULLY MODULE
+    app.add_handler(CommandHandler('bullystatus', bully.bullystatus, filters.User(username=MASTER_USERNAME)))
+    app.add_handler(CommandHandler('bullyenable', bully.bullyenable, filters.User(username=MASTER_USERNAME)))
+    app.add_handler(CommandHandler('bullydisable', bully.bullydisable, filters.User(username=MASTER_USERNAME)))
+    app.add_handler(CommandHandler('setbullylevel', bully.set_bullytolerance, filters.User(username=MASTER_USERNAME)))
+    app.add_handler(CommandHandler('getbullylevel', bully.get_bullytolerance, filters.User(username=MASTER_USERNAME)))
+
+
+
+
+
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Errors
+    app.add_error_handler(error) 
+
+    app.run_polling(poll_interval=1)
